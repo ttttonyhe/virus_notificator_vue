ffabe8a08fe3ac2f0ef4febf1ac631a79ec663bd
TonyHe <he@holptech.com>
2020-02-08 19:47:40 +0800

    8-2-2019

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,52 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const compress = new CompressionWebpackPlugin({
+  filename: info => {
+    return `${info.path}.gz${info.query}`
+  },
+  algorithm: 'gzip',
+  threshold: 10240,
+  test: new RegExp(
+    '\\.(' + ['js'].join('|') +
+    ')$'
+  ),
+  minRatio: 0.8,
+  deleteOriginalAssets: false
+})
+
 module.exports = {
   productionSourceMap: false,
+  devServer: {
+
+    before(app) {
+      app.get(/.*.(js)$/, (req, res, next) => {
+        req.url = req.url + '.gz';
+        res.set('Content-Encoding', 'gzip');
+        next();
+      })
+    }
+  },
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })]
+    },
+    plugins: [compress]
+  },
+  chainWebpack: config => {
+    config.optimization.minimize(true);
+    config.optimization.splitChunks({
+      chunks: 'all'
+    })
+  },
   css: {
-      extract: true
+    extract: true
   }
 }
